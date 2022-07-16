9ff6d340bd7e13efec4f8dec0e193ded36efd243
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-07-16 23:55:30 +0300

    test: UserController - implement unit test for exposed endpoint methods

--- src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -8,24 +8,31 @@ import com.example.demo.model.requests.CreateUserRequest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UserControllerTest {
     private UserController userController;
-    private UserRepository userRepository = mock(UserRepository.class);
-    private CartRepository cartRepository = mock(CartRepository.class);
-    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final CartRepository cartRepository = mock(CartRepository.class);
+    private final BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
 
     private static final String username = "test";
     private static final String password = "testPassword";
     private static final String hashedPassword = "thisIsHashed";
+    private static final long id = 1L;
+    private static final String unknownUsername = "unknownUsername";
 
     @Before
     public void setup() {
         userController = new UserController(userRepository, cartRepository, encoder);
+        mockEncoding(password, hashedPassword);
         //TODO: TestUtils is not working
         //TestUtils.injectObjects(userController, "userRepository", userRepository);
         //TestUtils.injectObjects(userController, "cartRepository", cartRepository);
@@ -34,7 +41,6 @@ public class UserControllerTest {
 
     @Test
     public void create_user_should_succeed() {
-        mockEncoding(password, hashedPassword);
         CreateUserRequest userRequest = createUserRequest();
 
         final ResponseEntity<User> response = userController.createUser(userRequest);
@@ -49,8 +55,79 @@ public class UserControllerTest {
     }
 
     @Test
-    public void find_user_by_id_should_succeed() {
+    public void create_user_with_no_password_should_return_400() {
+        CreateUserRequest userRequest = createUserRequest();
+        userRequest.setPassword(null);
 
+        final ResponseEntity<User> response = userController.createUser(userRequest);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void create_user_with_no_confirm_password_should_return_400() {
+        CreateUserRequest userRequest = createUserRequest();
+        userRequest.setConfirmPassword(null);
+
+        final ResponseEntity<User> response = userController.createUser(userRequest);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void create_user_with_password_less_than_7_characters_should_return_400() {
+        CreateUserRequest userRequest = createUserRequest();
+        userRequest.setPassword("123");
+
+        final ResponseEntity<User> response = userController.createUser(userRequest);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void create_user_with_confirm_password_different_than_password_return_400() {
+        CreateUserRequest userRequest = createUserRequest();
+        userRequest.setPassword("123");
+        userRequest.setConfirmPassword("1234");
+
+        final ResponseEntity<User> response = userController.createUser(userRequest);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void find_user_by_id_should_succeed() {
+        User user = createUser();
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+
+        final ResponseEntity<User> response = userController.findById(user.getId());
+
+        User foundUser = response.getBody();
+
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertEquals(username, foundUser.getUsername());
+        Assert.assertEquals(password, foundUser.getPassword());
+    }
+
+    @Test
+    public void find_user_by_username_should_succeed() {
+        User user = createUser();
+
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(user);
+
+        final ResponseEntity<User> response = userController.findByUserName(user.getUsername());
+
+        User foundUser = response.getBody();
+
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertEquals(username, foundUser.getUsername());
+        Assert.assertEquals(password, foundUser.getPassword());
+    }
+
+    @Test
+    public void find_user_by_username_should_return_not_found() {
+        final ResponseEntity<User> response = userController.findByUserName(unknownUsername);
+
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     private CreateUserRequest createUserRequest() {
@@ -61,8 +138,15 @@ public class UserControllerTest {
         return r;
     }
 
+    private User createUser() {
+        User u = new User();
+        u.setId(id);
+        u.setUsername(username);
+        u.setPassword(password);
+        return u;
+    }
+
     private void mockEncoding(String input, String output) {
         when(encoder.encode(input)).thenReturn(output);
     }
-
 }
